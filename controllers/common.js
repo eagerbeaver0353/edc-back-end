f47123b29f02a908942a42b47034b79af052b54c
adriandev0353 <adriandev0353@outlook.com>
2023-06-27 00:28:33 +1000

    fix some issues by upload

--- controllers/common.js
+++ controllers/common.js
@@ -4,15 +4,15 @@ const SubjectEmail = 'Horizon Tech signup verification code'
 
 module.exports.getLatestPosterImage = async (req, res, next) => {
   try {
-    console.log('AAA')
+    // console.log('AAA')
     const data = await EventMeeting.find()
       .sort({
         _id: -1,
       })
       .limit(1)
 
-    console.log(data)
-    res.redirect(`${process.env.UPLOAD_PATH}/${data[0].posterImage}`)
+    // console.log(data)
+    res.redirect(`${process.env.PUBLIC_PATH}${data[0].posterImage}`)
   } catch (err) {
     next(err)
   }
